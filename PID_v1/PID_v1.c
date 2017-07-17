14972508eb79acf0097037a8c86ac2f0437a979e
Eric Whitmire <t-erwhit@microsoft.com>
2017-07-17 14:32:14 -0700

    fixing header include name

--- PID_v1/PID_v1.c
+++ PID_v1/PID_v1.c
@@ -6,7 +6,7 @@
  * This Library is licensed under a GPLv3 License
  **********************************************************************************************/
 
-#include "pid.h"
+#include "PID_v1.h"
 void PID_Initialize(PidType* pid);
 
 /*Constructor (...)*********************************************************
